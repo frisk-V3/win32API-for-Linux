709686b218419b43e9721bb2c031268b2dc9130d
frisk-V3 <asahiharuto@outlook.jp>
2026-03-04 20:04:09 +0900

    Create win_sys_info.cpp

---
+++ GetSystemInfo/win_sys_info.cpp
@@ -0,0 +1,18 @@
+typedef struct _SYSTEM_INFO {
+    union {
+        DWORD dwOemId;
+        struct {
+            WORD wProcessorArchitecture;
+            WORD wReserved;
+        };
+    };
+    DWORD     dwPageSize;
+    LPVOID    lpMinimumApplicationAddress;
+    LPVOID    lpMaximumApplicationAddress;
+    DWORD_PTR dwActiveProcessorMask;
+    DWORD     dwNumberOfProcessors;
+    DWORD     dwProcessorType;
+    DWORD     dwAllocationGranularity;
+    WORD      wProcessorLevel;
+    WORD      wProcessorRevision;
+} SYSTEM_INFO;
