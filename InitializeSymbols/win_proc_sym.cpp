a89e5677da11cefff8618668604d43fa425b1e94
frisk-V3 <asahiharuto@outlook.jp>
2026-03-18 20:02:22 +0900

    Update win_proc_sym.cpp

--- InitializeSymbols/win_proc_sym.cpp
+++ InitializeSymbols/win_proc_sym.cpp
@@ -4,7 +4,7 @@
 
 #pragma comment(lib, "dbghelp.lib")
 
-
+//k
 extern "C" bool InitializeSymbols(HANDLE hProcess) {
     // ユーザー検索パス（カレントディレクトリや環境変数）を考慮して初期化
     return SymInitialize(hProcess, NULL, TRUE);
