3845cdc4a48a32c01ee7ff612ebcf12756aaba3d
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-10 14:44:04 +0900

    Create MB_Constants.cs

---
+++ Csharp/MessageBoxW/MB_Constants.cs
@@ -0,0 +1,13 @@
+namespace Win32Like.MessageBoxW
+{
+    internal static class MB
+    {
+        public const uint OK = 0x00000000;
+        public const uint OKCANCEL = 0x00000001;
+        public const uint YESNO = 0x00000004;
+
+        public const uint ICONINFO = 0x00000040;
+        public const uint ICONWARNING = 0x00000030;
+        public const uint ICONERROR = 0x00000010;
+    }
+}
