8c837f29bead2e127a0ce5271356212368d4ff0f
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-12 16:00:47 +0900

    Update GetProcAddress.hx

--- Haxe/sleep/GetProcAddress.hx
+++ Haxe/sleep/GetProcAddress.hx
@@ -1,7 +1,7 @@
-package LoadLibraryW;
+package;
 
 class GetProcAddress {
     public static function get(module:cpp.Pointer<Void>, name:String):cpp.Pointer<Void> {
-        return untyped __cpp__("GetProcAddress({0}, {1})", module, name);
+        return untyped __cpp__("::GetProcAddress((HMODULE){0}, {1})", module, name.__s);
     }
 }
